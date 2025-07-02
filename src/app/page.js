a154f4ae57554ab4f23480e5b08c2871bae16944
Balachandran <balachandran2729@gmail.com>
2025-07-02 10:17:37 +0530

    push & pull check

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,8 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
+          </a>
+            {/* <Image
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
@@ -40,15 +41,15 @@ export default function Home() {
               height={20}
             />
             Deploy now
-          </a>
-          <a
+          </a> */}
+          {/* <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             Read our docs
-          </a>
+          </a> */}
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
