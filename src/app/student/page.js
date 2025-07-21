7c39f00280448f276476e4a29f39b820076f8d38
shivaranjani-17 <shivaranjanishanthi@gmail.com>
2025-07-21 12:19:31 +0530

    Added bulk add modal

--- src/app/student/page.js
+++ src/app/student/page.js
@@ -1,6 +1,7 @@
 "useclient"
 import React from "react"
 import NavBar from "../navBar/page";
+import BulkModal from "./bulkModal";
 
 export default function StudentPage() {
   return (
@@ -9,6 +10,7 @@ export default function StudentPage() {
       {/* Main Content */}
       <h1 className="text-2xl font-bold mb-4">Student Dashboard</h1>
       <p>This is the student dashboard where you can view your profile, apply for placements, and manage your applications.</p>
+      <BulkModal />
       {/* Add more student-specific features here */}
     </div>
   );
