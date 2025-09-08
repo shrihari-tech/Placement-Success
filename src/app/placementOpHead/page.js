d12893b678825b5ed74605142c3cb222560314b5
shrihari-tech <bhshrihari@gmail.com>
2025-09-08 11:21:48 +0530

    removed both changes

--- src/app/placementOpHead/page.js
+++ src/app/placementOpHead/page.js
@@ -1,18 +1,4 @@
-"use client";
 
-import React from "react";
-
-export default function PlacementOpHeadPage() {
-  return (
-    <div>
-      <h1>Placement Operation Head</h1>
-      <p>This is the placement operation head page.</p>
-    </div>
-  );
-}
-
-
-// placementOpHead/page.js
 "use client";
 import { redirect } from 'next/navigation';
 
